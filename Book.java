2c45683f2b3a83399706634133b310374357d754
AlexMagl <alex.maglakelidze@gmail.com>
2024-04-01 11:21:39 +0400

    Files upload

---
+++ Book.java
@@ -0,0 +1,19 @@
+public class Book {
+    private String title, author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
---
+++ LMSTester.java
@@ -0,0 +1,17 @@
+public class LMSTester {
+    public static void main(String[] args) {
+        LMS testLibrary = new LMS();
+
+        Book tsikara  = new Book();
+        tsikara.setTitle("წიქარა");
+        tsikara.setAuthor("N/A");
+
+        Book oop = new Book();
+        oop.setTitle("Object Oriented Programming");
+        oop.setAuthor("Paata Gogishvili");
+
+        testLibrary.addBook(tsikara);
+        testLibrary.addBook(oop);
+
+    }
+}
